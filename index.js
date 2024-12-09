2b82dab329c8b9e820d82fc35dc735546ccb0974
chetan panara <chetanpanara88@gmail.com>
2024-12-09 23:50:33 +0530

    commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require('cors')
 const dotenv = require('dotenv')
+const YoutubevideosModel = require('./Model/Youtubevideos');
 
 dotenv.config()
 
@@ -13,11 +14,11 @@ mongoose.connect(process.env.Connection)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error(err));
 
-const YoutubevideosModel = require('./Model/Youtubevideos');
+
 
 app.get('/data', (req, res) => { 
   YoutubevideosModel.find({})
-  .then(ck =>res.json(ck))
+  .then(result =>res.json(result))
   .catch(err=>res.json(err))
 })
 
